fadf52d4918376b8bde4ef2b31d9e6f2b52965a9
Chuiko Egor <egorchuiko7@gmail.com>
2025-11-09 02:02:13 +0300

    Add custom exception

---
+++ src/main/java/by/chaika19/task2/exception/TextException.java
@@ -0,0 +1,17 @@
+package by.chaika19.task2.exception;
+
+public class TextException extends Exception {
+    public TextException() {}
+
+    public TextException(String message) {
+        super(message);
+    }
+
+    public TextException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TextException(Throwable cause) {
+        super(cause);
+    }
+}
