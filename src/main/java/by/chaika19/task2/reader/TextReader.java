d629fe3d14dff363f040bc18be79f7fba26968ef
Chuiko Egor <egorchuiko7@gmail.com>
2025-11-09 02:01:52 +0300

    Add reader

---
+++ src/main/java/by/chaika19/task2/reader/TextReader.java
@@ -0,0 +1,8 @@
+package by.chaika19.task2.reader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TextReader {
+    private static final Logger logger = LogManager.getLogger(TextReader.class);
+}
