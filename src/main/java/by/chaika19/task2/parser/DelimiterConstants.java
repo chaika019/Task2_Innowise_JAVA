5277c55824018f9cf1ecb91a714a9b64941b97a8
Chuiko Egor <egorchuiko7@gmail.com>
2025-11-14 23:26:24 +0300

    Extract composition delimiters to DelimiterConstants

---
+++ src/main/java/by/chaika19/task2/parser/DelimiterConstants.java
@@ -0,0 +1,10 @@
+package by.chaika19.task2.parser;
+
+public final class DelimiterConstants {
+    private DelimiterConstants() {}
+
+    public static final String TEXT_DELIMITER = "\n\n";
+    public static final String SENTENCE_DELIMITER = " ";
+    public static final String PARAGRAPH_DELIMITER = " ";
+    public static final String LEXEME_DELIMITER = "";
+}
